baa4b4f0af1c5320484e204db68359f7e23b7f3c
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-15 17:17:47 +0100

    feat() Include templating
    Include handlebars for templating
    
    Section 5, Lesson 43

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 
-
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (requeste, response) => {
   //response.send('<h1>Hello Express!</h1>');
-  response.send({name: 'Alberto', likes: ['chess', 'football']});
+  //response.send({name: 'Alberto', likes: ['chess', 'football']});
+  response.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Hello friends'
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
