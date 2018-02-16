f3d5995fa6792c4b9e94588e513fc4bf274f41fb
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-16 10:10:19 +0100

    feat() Include middlewares
    Include middlewares and maintenance page
    
    Section 5, Lesson 45

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
 hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 app.set('view engine', 'hbs');
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}\n`;
+  fs.appendFile('server.log', log, (err) => {
+    if (err) {
+      console.log('Unable to append log to the file', err);
+    }
+  });
+  next();
+});
 
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 hbs.registerHelper('screamIt', (text) => text.toUpperCase());
